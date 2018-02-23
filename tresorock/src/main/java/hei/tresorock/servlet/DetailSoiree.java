4ec643afc0286960fdd2bb1cc6c743a622ebf592
Gaëtan Deschamps <gaetan.deschamps@hei.yncrea.fr>
2018-02-23 08:27:04 +0100

    .

--- tresorock/src/main/java/hei/tresorock/servlet/DetailSoiree.java
+++
@@ -1,6 +0,0 @@
-package hei.tresorock.servlet;
-
-import javax.servlet.http.HttpServlet;
-
-public class DetailSoiree extends HttpServlet {
-}
--- tresorock/src/main/java/hei/tresorock/servlet/test.java
+++
@@ -1,7 +0,0 @@
-package hei.tresorock.servlet;
-
-public class test {
-
-    //Commentaire de Test
-
-}
